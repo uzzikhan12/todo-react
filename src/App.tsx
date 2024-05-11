40531f689c032bb736c6ebadb1bb01f47b6fa197
uzzikhan12 <hk33636625@gmail.com>
2024-05-12 04:42:17 +0500

    Complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,88 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
 
 function App() {
+  let [text,setText] = useState<any>();
+  let [list,setList] = useState<any>([]);
+
+  function del(i:any) {
+    list.splice(i,1);
+    setList([...list]);
+  }
+
+  function edit (i:any) {
+    let newTxt = prompt("Enter new text");
+    list[i] = newTxt;
+    setList([...list]);
+  }
+
+  function delAll () {
+    setList([]);
+  }
+
+  document.body.style.backgroundColor = "#3ebd9d";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className='d-flex mt-3 w-50 m-auto justify-content-center'>
+
+        <input type="text" onChange={((e) => {
+          setText(e.target.value);
+        })} 
+        className='me-2 w-50 p-1 ps-2'/>
+
+        <button onClick={() => {
+          list.push(text);
+          setList([...list]);
+        }}
+        className='btn btn-primary me-2'>
+          Add
+        </button>
+
+        <button onClick={() => {
+          delAll()
+        }}
+        className='btn btn-danger'>
+          Delete All
+        </button>
+
+        </div>
+
+        <div className='mt-5'>
+
+        {list.map((e:any, i:any) => {
+          return(
+            <p key={i}
+            className='pera'>{e}
+            
+              <div>
+
+                <button onClick={() => {
+                  edit(i)
+                }}
+                className='btn btn-success'>
+                  Edit
+                </button>
+            
+                <button onClick={() => {
+                  del(i)
+                }}
+                className='btn btn-dark ms-2'>
+                  Delete
+                </button>
+            
+              </div>
+            
+            </p>
+          )
+        })}
+
+        </div>
+
     </div>
   );
 }
